8c3fd858fa526f3d6c8acfa49ac5cc4892a01324
ank1331 <ankitsharma133189@gmail.com>
2023-09-09 19:24:11 +0530

    SignUp Page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function SignUp() {
+  const [formData, setFormdata] = useState({})
+  const handleChange = (e)=>{
+    setFormdata({...formData, [e.target.id]:e.target.value})
+  }
+
+  const handleSubmit= async(e)=>{
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      SignUp
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder = "Username" id="username" className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+        <input type="text" placeholder = "Email" id="email" className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+        <input type="text" placeholder = "Password" id="password" className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
+      </form>
+      <div className="flex gap-2 mt-5">
+      <p>Have an account?</p>
+      <Link to="/Sign-in">
+        <span className='text-red-500'>Sign In</span>
+      </Link>
+      </div>
     </div>
   )
 }
