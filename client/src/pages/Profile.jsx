c3f187b9c0625318bbf59e0623d6fe50bdbcfe8a
ank1331 <ankitsharma133189@gmail.com>
2023-09-13 08:18:11 +0530

    Updating the profile picture

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -46,7 +46,7 @@ console.log(formData)
       <h1 className='text-3xl font-semibold text-center my-7'> Profile</h1>
       <form className="flex flex-col gap-4">
         <input type="file" ref = {fileRef} hidden accept = "image/*" onChange={(e)=>setImage(e.target.files[0])}/>
-        < img src={currentUser.profilePicture} alt="profile" className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2" onClick={()=>fileRef.current.click()}/>
+        < img src={formData.profilePicture||currentUser.profilePicture} alt="profile" className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2" onClick={()=>fileRef.current.click()}/>
         <p className='text-sm self-center'>
           {imageError ? (
             <span className='text-red-700'>
