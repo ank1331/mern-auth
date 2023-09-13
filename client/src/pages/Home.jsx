3091702981a1ab8c20a484597d21ffc408373ba9
ank1331 <ankitsharma133189@gmail.com>
2023-09-14 01:16:28 +0530

    Add complete home and about pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl text-center font-bold mb-4 text-slate-800'>Welcome to my Authentication App</h1>
+      <p className='mb-2 text-slate-700' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nam error explicabo dolorem maiores, possimus cum illo optio iure soluta tempore! Odit numquam necessitatibus ut culpa ad, aliquid placeat. Assumenda repellendus necessitatibus debitis eligendi iure veniam culpa facilis dicta cumque, officia nihil aliquid minus voluptates sint temporibus nobis maiores commodi voluptatibus. </p>
+      <p className='mb-4 text-slate-700' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nam error explicabo dolorem maiores, possimus cum illo optio iure soluta tempore! Odit numquam necessitatibus ut culpa ad, aliquid placeat. Assumenda repellendus necessitatibus debitis eligendi iure veniam culpa facilis dicta cumque, officia nihil aliquid minus voluptates sint temporibus nobis maiores commodi voluptatibus. Nostrum totam molestias magni eligendi eaque, ipsam nisi consectetur, impedit ducimus voluptatum fugiat nobis doloremque voluptatibus sint expedita maiores blanditiis, quae adipisci odit accusantium ratione aperiam. Ex consectetur et asperiores verois at quos!</p>
+
     </div>
   )
 }
